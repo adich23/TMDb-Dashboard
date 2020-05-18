d05fc14f8e46981f1596fab151da26ae9e0452d3
Aditya Choudhary <webmasteradi@gmail.com>
2020-05-18 00:59:42 -0400

    Add parallel-plot function

--- static/part-1.js
+++ static/part-1.js
@@ -1,12 +1,48 @@
 
 
-var category = 'Consolidated';//'Science Fiction';
+var category = 'Comedy';//'Science Fiction';
 
 plot_scatter('/draw2dScatterPlot/'+category,category);
 
 plot_boxplot('/drawBoxPlot/'+category,category);
+// always call box-plot first
+plot_parallelplot('/drawParallelPlot/'+category,category);
 
-function draw_box_plot(dictData, chartTitle,divId) {
+var boxplot_data;
+
+// for box-plot and parallel-plot
+// var colorScale;
+
+const btn = document.querySelector('#btn');
+const btn1 = document.querySelector('#btn_1');
+// handle click button
+btn.onclick = function () {
+    draw_box_plot(boxplot_data,0,category,'div5');
+    // var selectedValue;
+    // const rbs = document.querySelectorAll('input[name="choice"]');
+    // for (const rb of rbs) {
+    //     if (rb.checked) {
+    //         selectedValue = rb.value;
+    //         break;
+    //     }
+    // }
+    
+};
+
+btn1.onclick = function () {
+    draw_box_plot(boxplot_data,1,category,'div5');
+};
+
+
+
+
+
+
+
+
+
+
+function draw_box_plot(dictData, feature_num, chartTitle, divId) {
     
     // set the dimensions and margins of the graph
     d3.select('#chart'+divId).remove();
@@ -15,11 +51,12 @@ function draw_box_plot(dictData, chartTitle,divId) {
     height = 400 - margin.top - margin.bottom;    
   
     var barWidth = 30;
-    var div = "#"+divId
+    var div = "#"+divId;
     console.log(div)
 
     // Generate five 100 count, normal distributions with random means
-    var groupCounts = JSON.parse(dictData);
+    var listGroupCounts = JSON.parse(dictData);
+    var groupCounts = listGroupCounts[+feature_num];
     var globalCounts = [];
     // var meanGenerator = d3.randomUniform(10);
     // for(i=0; i <= 5; i++) {
@@ -45,7 +82,7 @@ function draw_box_plot(dictData, chartTitle,divId) {
 
     // Setup a color scale for filling each box
     var colorScale = d3.scaleOrdinal(d3.schemeCategory10)
-    .domain(Object.keys(groupCounts));
+    .domain(Object.keys(groupCounts).sort());
 
     // Prepare the data for the box plots
     var boxPlotData = [];
@@ -111,10 +148,11 @@ function draw_box_plot(dictData, chartTitle,divId) {
 
     // tooltip mouseover event handler
     var tipMouseover = function(d) {
-        console.log("MOuseOver")
+        // console.log("MOuseOver")
         // console.log(d)
-        var html  =  "<b> Median " + d.quartile[1] + "</b><br/>"+
-                    "<b>Min. " + d.whiskers[0] + "</b><br/>"+ "<b/> Max. " + d.whiskers[1] + "</b>";
+        var html  = "<b>Min. " + d.whiskers[0] + "</b><br/>"+  
+                    "<b> Median " + d.quartile[1] + "</b><br/>"+
+                    "<b/> Max. " + d.whiskers[1] + "</b>";
 
         tooltip.html(html)
             .style("left", (d3.event.pageX + 15) + "px")
@@ -203,9 +241,28 @@ function draw_box_plot(dictData, chartTitle,divId) {
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(xScale));
 
+    svg.append("text")             
+      .attr("transform",
+            "translate(" + (width/2) + " ," + 
+                           (height + margin.top + 5) + ")")
+      .style("text-anchor", "middle")
+      .text("Years");
+
+    let feature_map = new Map();
+    feature_map.set('0', 'Popularity');
+    feature_map.set('1', 'Runtime');
+
     // Add the Y Axis
     svg.append("g")
     .call(d3.axisLeft(yScale));
+
+    svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", -30)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text(feature_map.get(feature_num)); 
         
     function boxQuartiles(d) {
         return [
@@ -226,6 +283,201 @@ function draw_box_plot(dictData, chartTitle,divId) {
 
 
 }
+
+
+function draw_parallel_plot(dfData, chartTitle,divId) {
+    
+    d3.select('#parallel'+divId).remove();
+
+    var margin = {top: 30, right: 10, bottom: 10, left: 10};
+    var width = 960 - margin.left - margin.right;
+    var height = 400 - margin.top - margin.bottom;
+
+    sample_data = JSON.parse(dfData)
+
+    var x = d3.scalePoint().range([0, width]).padding(1),
+        y = {};
+
+    var line = d3.line(),
+        axis = d3.axisLeft(),
+        background,
+        foreground;
+    
+    var dimensions = null;
+    
+    // const svg = d3.select(div).append()(width + margin.left + margin.right, height+margin.top + margin.bottom)
+    // .attr('id', 'parallel'+divId));
+    
+
+    var div = "#"+divId;
+    console.log(div)
+    var svg = d3.select(div).append("svg").attr('id', 'parallel'+divId)
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+
+        const svg_adjusted = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+
+    years_list = []
+    sample_data.forEach(function(d){
+        years_list.push(d.year);
+      })
+    years_set = d3.set(years_list.sort())
+    years_list = Array.from(years_set);
+    var colorScale = d3.scaleOrdinal(d3.schemeCategory10)
+    .domain(years_list);
+    // Extract the list of dimensions and create a scale for each.
+    x.domain(dimensions = d3.keys(sample_data[0]).filter(function(d) {
+        
+        // if (d == 'year') {
+        //     return y[d] = d3.scalePoint()
+        //     .domain(years_list)
+        //     // .domain(d3.extent(sample_data, function(p) { p[d]; }))
+        //     .range([0,height]);
+        // }
+        if (d == "year") {
+            return (y[d] = d3.scaleLinear()
+            .domain(d3.extent(sample_data, function(p) { return parseInt(p[d]); }))
+            .range([height, 0]));
+        }
+        if (d !="name" && d!= "year") {
+            return (y[d] = d3.scaleLinear()
+            .domain(d3.extent(sample_data, function(p) { return +p[d]; }))
+            .range([height, 0]));
+        }
+        // return d != "name" && (y[d] = d3.scaleLinear()
+        //     .domain(d3.extent(sample_data, function(p) { return +p[d]; }))
+        //     .range([height, 0]));
+    }));
+
+    // y['year'] = d3.scalePoint()
+    // .domain(years_list)
+    // .range([0, height]);
+
+    // Add the tooltip container to the vis container
+    // it's invisible and its position/contents are defined during mouseover
+    var tooltip = d3.select(div).append("div")
+        .attr("class", "tooltip4")
+        .style("opacity", 0);
+
+    // tooltip mouseover event handler
+    var tipMouseover = function(d) {
+        // console.log("MOuseOver")
+        console.log(d)
+        var html  =  "<b>" + d.name+ "</b>";
+
+        tooltip.html(html)
+            .style("left", (d3.event.pageX + 10) + "px")
+            .style("top", (d3.event.pageY - 2) + "px")
+        .transition()
+            .duration(200) // ms
+            .style("opacity", .8) // started as 0!
+
+    };
+    // tooltip mouseout event handler
+    var tipMouseout = function(d) {
+        tooltip.transition()
+            .duration(300) // ms
+            .style("opacity", 0); // don't care about position!
+    };
+
+
+
+
+
+    // Add grey background lines for context.
+    background = svg_adjusted.append("g")
+        .attr("class", "background")
+        .selectAll("path")
+        .data(sample_data)
+        .enter().append("path")
+        .attr("d", path);
+
+    // Add foreground lines for focus.
+    foreground = svg_adjusted.append("g")
+        .attr("class", "foreground")
+        .selectAll("path")
+        .data(sample_data)
+        .enter().append("path")
+        .attr("d", path)
+        .style("stroke", function(d) { return colorScale(d.year); })
+        // .on("mouseover", highlight)
+        // .on("mouseleave", doNotHighlight );
+        .on("mouseover", tipMouseover)
+        .on("mouseout", tipMouseout);
+
+    // Add a group element for each dimension.
+    const g = svg_adjusted.selectAll(".dimension")
+        .data(dimensions)
+        .enter().append("g")
+        .attr("class", "dimension")
+        .attr("transform", function(d) { return "translate(" + x(d) + ")"; });
+
+    // Add an axis and title.
+    g.append("g")
+        .attr("class", "axis")
+        .each(function(d) { 
+            d3.select(this).call(axis.scale(y[d]));
+            if(d=='year') {
+                d3.select(this).call(axis.ticks(5));
+                d3.select(this).call(axis.tickFormat(x => x.toString()));
+            }
+        })
+        .append("text")
+        .style("text-anchor", "middle")
+        .attr("y", -9)
+        .text(function(d) { return d; });
+
+    // Add and store a brush for each axis.
+    g.append("g")
+        .attr("class", "brush")
+        .each(function(d) { 
+            d3.select(this).call(y[d].brush = d3.brushY()
+                .extent([[-10,0], [10,height]])
+                .on("brush", brush)           
+                .on("end", brush)
+                )
+            })
+        .selectAll("rect")
+        .attr("x", -8)
+        .attr("width", 16);
+
+    // Returns the path for a given data point.
+    function path(d) {
+        return line(dimensions.map(function(p) { return [x(p), y[p](d[p])]; }));
+    }
+
+    // Handles a brush event, toggling the display of foreground lines.
+    function brush() {  
+        var actives = [];
+        svg.selectAll(".brush")
+        .filter(function(d) {
+                y[d].brushSelectionValue = d3.brushSelection(this);
+                return d3.brushSelection(this);
+        })
+        .each(function(d) {
+            // Get extents of brush along each active selection axis (the Y axes)
+                actives.push({
+                    dimension: d,
+                    extent: d3.brushSelection(this).map(y[d].invert)
+                });
+        });
+        
+        var selected = [];
+        // Update foreground to only display selected values
+        foreground.style("display", function(d) {
+            let isActive = actives.every(function(active) {
+                let result = active.extent[1] <= d[active.dimension] && d[active.dimension] <= active.extent[0];
+                return result;
+            });
+            // Only render rows that are active across all selectors
+            if(isActive) selected.push(d);
+            return (isActive) ? null : "none";
+        });
+    }
+}
+
 
 function draw_scatter_2d(dfData, chartTitle,divId) {
 
@@ -396,10 +648,30 @@ function plot_boxplot(url, selectedValue) {
         success: function(result) {
             console.log("3-D Python call Success")
             // console.log(result)
-            draw_box_plot(result,selectedValue,'div5')
+            boxplot_data = result
+            draw_box_plot(boxplot_data,0,selectedValue,'div5')
         },
         error: function(result) {
             $("#div5").html(result);
+        }
+    });
+}
+
+function plot_parallelplot(url, selectedValue) {
+    $.ajax({
+        type: 'GET',
+        url: url,
+        contentType: 'application/json; charset=utf-8',
+        xhrFields: {
+          withCredentials: false
+        },
+        success: function(result) {
+            console.log("3-D Python call Success")
+            // console.log(result)
+            draw_parallel_plot(result,selectedValue,'div6')
+        },
+        error: function(result) {
+            $("#div6").html(result);
         }
     });
 }
